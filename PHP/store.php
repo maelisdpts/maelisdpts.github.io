24248702dbaceb7564c1f2c183bad2e3fdff8ac9
maelisdpts <73491941+maelisdpts@users.noreply.github.com>
2021-02-10 15:54:31 +0100

    Update store.php

--- PHP/store.php
+++ PHP/store.php
@@ -1,55 +1,38 @@
-<?php
+<?php 
 
-// Renvoie le code html de la page store.html
+$api = file_get_contents('https://api.api2cart.com/v1.1/product.list.json?start=0&count=50&api_key=4818b589810aa7b9bebc37693baa7ff3&store_key=ed58a22dfecb405a50ea3ea56979360d');
 
-session_start();
+$api = json_decode($api, true);
 
-    $bdd = new PDO('mysql:host=localhost;dbname=website;charset=utf8', 'root', 'root');
-    if (isset($_POST['Produit'])) {
-        if (isset($_POST['Prix1'])) {
-            if($_POST['Produit'] != 'e'){
-                $rep = $bdd->query('SELECT * FROM produits WHERE prix BETWEEN ' . $_POST['Prix1'] . ' AND ' . $_POST['Prix2'] . ' AND categorie=\'' .$_POST['Produit']. '\'');
-            }else{
-                $rep = $bdd->query('SELECT * FROM produits WHERE prix BETWEEN ' . $_POST['Prix1'] . ' AND ' . $_POST['Prix2']);
-            }
-        }else {
-            if($_POST['Produit'] != 'e'){
-                $rep = $bdd->query('SELECT * FROM produits WHERE categorie=\'' .$_POST['Produit']. '\'');
-            }else{
-                $rep = $bdd->query('SELECT * FROM produits');
-            }
-        };
-    } else {
-        if (isset($_POST['Prix1'])) {
-            $rep = $bdd->query('SELECT * FROM produits WHERE prix BETWEEN '. $_POST['Prix1'] .' AND ' . $_POST['Prix2']);
-        } else {
-            $rep = $bdd->query('SELECT * FROM produits');
-        };
+$donnee = $api['result']['product'];
+//echo($api['result']['product'][0]['name']);
+
+$column = 1;
+$row = 0;
+$html = '<div class="products"><div class="lig'. $row . '">';
+
+
+for ($i=0; $i<=36; $i++) { 
+    if ($row == 0 && $column == 5) {
+        $row += 1;
+        $column = 1;
+        $html .= '</div><div class="lig'. $row .'">';
+    }else if ($row != 0 && $column == 5) {
+        $row += 1;
+        $column = 1;
+        $html .= '</div><div class="lig'. $row .'">';
     };
+    $html .= '<div class="product" onclick="produit('. $i .')"><div class="img" ><img class="img" src="../ASSETS/produit.jpg"> </div> <div class="text"><h2>' . $donnee[$i]["name"] . '</h2><h3>'. $donnee[$i]["price"] . ' € </h3></div></div>';
+    $column += 1;
+};
 
-    $column = 1;
-    $row = 0;
-    $html = '<div class="products"><div class="lig'. $row . '">';
 
-    while ($donnee=$rep->fetch()) {
-        if ($row == 0 && $column == 5) {
-            $row += 1;
-            $column = 1;
-            $html .= '</div><div class="lig'. $row .'">';
-        }else if ($row != 0 && $column == 5) {
-            $row += 1;
-            $column = 1;
-            $html .= '</div><div class="lig'. $row .'">';
-        };
-        $nom = $donnee["ID"];
-        $html .= '<div class="product" onclick="produit('. $nom .')"><div class="img" ><img class="img" src="'. $donnee['Image'] .'"> </div> <div class="text"><h2>' . $donnee["Categorie"] . '</h2><h3>'. $donnee["Prix"] . ' € </h3></div></div>';
-        $column += 1;
-    }
+if ($row == 0 && $column == 1){
+    $html .= '<h3> Aucun produit ne correspond à votre recherche...';
+}
+$html .= '</div></div>';
 
-    if ($row == 0 && $column == 1){
-        $html .= '<h3> Aucun produit ne correspond à votre recherche...';
-    }
-    $html .= '</div></div>';
 
-    echo($html);
+echo($html);
+
 ?>
