f0d8c3960a007a4b8017c27a73067ddfd5f6063c
maelisdpts <73491941+maelisdpts@users.noreply.github.com>
2021-02-10 15:50:49 +0100

    Update footer.php

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,16 +1,22 @@
 <?php
 
-echo '<nav class="nav">
-    <ul>
-        <li><a href="main.html"> Home </a></li>
-        <li><a href="#"> Store </a></li>
-        <li><a href="#"> Blog </a></li>
-        <li><a href="main.html"> About </a></li>
-    </ul>
-</nav>
-<div class="newsletter">
-    <h5> GET THE NEW COLLECTIONS FIRST !</h5>
-    <input type="text" placeholder="Entrer votre adresse e-mail"> 
-</div>';
+echo '
+<h1> Recevez nos collections en premier !</h1>
+        <div class="newsletter">
+            <input class="inscription" type="text" placeholder="Entrer votre adresse e-mail">
+            <input class="submit" type="submit" value="S\'inscrire" />
+        </div>
+        <div class="footer_menu">
+            <nav class="nav">
+                <ul>
+                    <li><a href="main.html"> Home </a></li>
+                    <li><a href="#"> Store </a></li>
+                    <li><a href="#"> Blog </a></li>
+                    <li><a href="main.html"> About </a></li>
+                </ul>
+            </nav>
+            <a href="main.html" class="logo"> CODESS </a>
+        </div>
+        <hr>';
 
 ?>
